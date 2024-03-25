5695dc2ce3747a350edeb15e04c868a24c48f7f3
조예성 <seong0241@gmail.com>
2024-03-25 20:42:03 +0900

    commit

--- 백준/<1002>/<1002>.c
+++ 백준/<1002>/<1002>.c
@@ -1,35 +1,8 @@
-#include <stdio.h> 
-#include <stdlib.h>
-#include <math.h> 
+#include <stdio.h>
 
-int main(void)
+int main()
 {
-   int n;
-   scanf("%d", &n);
-
-   for(int i=0; i<n; i++) {
-      int x1, y1, r1, x2, y2, r2, z;
-      float a;
-      
-      scanf("%d %d %d %d %d %d",&x1,&y1,&r1,&x2,&y2,&r2);
-
-      if(x1==x2 && y1==y2 && r1==r2) { z=-1; }
-      else {
-         a = sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-         if(r1+r2 < a) { z = 0;   }
-         else {
-            if(r1+r2 == a) { z = 1; }
-            else{
-               if(abs(r1-r2) == a) {z = 1;   }
-               else {
-                  if(abs(r1-r2) < a) { z = 2; }
-                  else {z = 0;} 
-               }
-            }
-         }
-      }
-       printf("%d\n",z);
-   }
+    printf("Hello World!");
+}
    
        
-}
